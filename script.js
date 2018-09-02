5e2f3a3db5c50a8e704e2e9a90f60b962a005cf5
zamhaq <haque864@gmail.com>
2018-09-02 18:34:05 +0530

    Requirements change: Display two cats with counter

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const cat = document.getElementById('cat');
-const clickCounter = document.getElementById('clickcounter');
-let clickcount = 0;
-cat.addEventListener('click',(e) => {
-    clickcount++;
-    clickCounter.innerText = clickcount;
+const cat1 = document.getElementById("cat1");
+const cat2 = document.getElementById("cat2");
+const clickCountercat1 = document.getElementById("clickcountercat1");
+const clickCountercat2 = document.getElementById("clickcountercat2");
+let clickcount = [0, 0];
+cat1.addEventListener("click", e => {
+  clickcount[0]++;
+  clickCountercat1.innerText = clickcount[0];
+});
+cat2.addEventListener("click", e => {
+  clickcount[1]++;
+  clickCountercat2.innerText = clickcount[1];
 });
 console.log(cat);
